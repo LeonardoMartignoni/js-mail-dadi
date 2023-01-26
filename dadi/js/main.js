ced0f0535ac9522436b725fc6af2ff7d0d326185
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-01-26 16:57:29 +0100

    First Commit

---
+++ dadi/js/main.js
@@ -0,0 +1,8 @@
+// Genero un numero casuale da 1 a 6 per il giocatore
+// Genero un numero casuale da 1 a 6 per il computer
+// SE il numero del giocatore è maggiore di quello del computer
+//      Stampo un messaggio di vincita da parte del giocatore
+// ALTRIMENTI SE il numero del giocatore e del computer sono uguali
+//      Stampo un messaggio di pareggio
+// ALTRIMENTI
+//      Stampo un messaggio di vincita del computer
