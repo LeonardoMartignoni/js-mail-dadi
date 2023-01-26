b571a62a5075cbe1ed36e066d081ea2f481cbfe4
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-01-26 17:36:21 +0100

    Manipulated DOM

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,34 +1,43 @@
 // Creo una lista di email accettate
 const acceptedEmail = ['123@123.com', '456@456.com', '789@789.com'];
 
-// Chiedo l'email all'utente
-const userEmail = prompt('Inserisci la tua email')
+// Prendo il bottone per l'invio
+const sendButton = document.getElementById('send_button');
 
-// Imposto una variabile per sapere se l'email è registrata
-let userHasPermission = false;
+// Prendo il paragrafo per i risultati
+const resultMessage = document.getElementById('result_message');
 
-// Creo un for che scorra nella lista
-for (i = 0; i < acceptedEmail.length; i++) {
-    console.log(acceptedEmail[i]);
+sendButton.addEventListener(
+    'click',
 
-    // SE l'email dell'utente è nella lista
-    if (userEmail == acceptedEmail[i]) {
+    function () {
+        // Prendo l'email dell'utente
+        const userEmail = document.getElementById('user_email').value;
 
-        //      Modifico lo stato della variabile
-        userHasPermission = true;
+        // Imposto una variabile per sapere se l'email è registrata
+        let userHasPermission = false;
+
+        // Creo un for che scorra nella lista
+        for (i = 0; i < acceptedEmail.length; i++) {
+
+            // SE l'email dell'utente è nella lista
+            if (userEmail == acceptedEmail[i]) {
+
+                //      Modifico lo stato della variabile
+                userHasPermission = true;
+            }
+        }
+
+        // SE l'utente ha i permessi
+        if (userHasPermission == true) {
+            //      Stampo un messaggio di esito positivo
+            resultMessage.innerHTML = 'Accesso consentito'
+        }
+
+        // ALTRIMENTI
+        else {
+            //      Stampo un messaggio di esito negativo
+            resultMessage.innerHTML = 'Accesso negato'
+        }
     }
-}
-
-// SE l'utente ha i permessi
-if (userHasPermission == true) {
-    //      Stampo un messaggio di esito positivo
-    console.log('Accesso consentito');
-}
-
-// ALTRIMENTI
-else {
-    //      Stampo un messaggio di esito negativo
-    console.log('Accesso negato');
-}
-
-
+)
