0b59ddccf8b2c5bf6dc495dfc84e6f41c283b13c
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-01-26 17:01:05 +0100

    Done Structure

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,8 +1,25 @@
 // Genero un numero casuale da 1 a 6 per il giocatore
+const userDice = Math.floor(Math.random() * 6) + 1;
+console.log(userDice);
+
 // Genero un numero casuale da 1 a 6 per il computer
+const botDice = Math.floor(Math.random() * 6) + 1;
+console.log(botDice);
+
 // SE il numero del giocatore è maggiore di quello del computer
-//      Stampo un messaggio di vincita da parte del giocatore
+if (userDice > botDice) {
+    //      Stampo un messaggio di vincita da parte del giocatore
+    console.log('Hai vinto!');
+}
+
 // ALTRIMENTI SE il numero del giocatore e del computer sono uguali
-//      Stampo un messaggio di pareggio
+else if (userDice == botDice) {
+    //      Stampo un messaggio di pareggio
+    console.log('Pareggio!');
+}
+
 // ALTRIMENTI
-//      Stampo un messaggio di vincita del computer
+else {
+    //      Stampo un messaggio di vincita del computer
+    console.log('Hai perso, riprova!');
+}
